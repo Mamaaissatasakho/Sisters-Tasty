279bacfba1f9e6c261bf519f7f09e13da420a282
Aicha <aicha.sakho06@gmail.com>
2024-06-09 22:44:28 +0200

    code source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import sisterPng from "./sister.png"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <img src={sisterPng} alt="sister png" />
+        <h1>La gastronomie de Dakar à Paris</h1>
+        <h2>Sisters-Tasty, c'est un restaurant gastronomique tenu par deux soeurs françaises d'origines sénégalaises qui vous font voyager avec les plats de leurs pays bien aimés</h2>
     </div>
   );
 }
